9fe588b25928df7367a343ff213c7aa31d202157
Basil L. Contovounesios <contovob@tcd.ie>
2015-04-02 13:37:41 +0100

    Commented code

--- app/server.R
+++ app/server.R
@@ -1,45 +1,73 @@
 library(shiny)
 
-shinyServer(function(input,output){
+shinyServer(function(input, output){
+
+  # Choose continuous distribution type
   dat <- reactive({
-    dist <- switch(input$dist,norm=rnorm, unif=runif, exp=rexp)
-    
+    dist <- switch(input$dist,
+                   norm = rnorm,
+                   unif = runif,
+                   exp  = rexp)
     dist(input$n)
   })
 
-  
   output$plot <- renderPlot({
-    dist <- input$dist
-    n <- input$n
-    ntrials <- input$ntrials
-    psucc <- input$psucc
-    lambda <- input$lambda
-    
-    
-    if(input$distType=="Continuous"){
-      hist(dat(),col="orange",xlab="Observations",main=paste('r', dist, '(', n, ')', sep=''), prob=TRUE)
-      if(input$density == TRUE) lines(density(dat(),adjust=input$bw),lwd=2)
-    }
-    else{
-      if(input$dist_disc=="bino"){
-        bindat<-rbinom(n,ntrials,psucc)         
-        hist(bindat,col="orange", main=paste('r', dist, '(', n, ')', sep=''), prob=TRUE)
-        if(input$density == TRUE) lines(density(bindat,adjust=input$bw),lwd=2)
+
+    dist    <- input$dist       # Distribution type
+    n       <- input$n          # Sample size
+    ntrials <- input$ntrials    # Trials for binomial
+    psucc   <- input$psucc      # Probability of success for binomial
+    lambda  <- input$lambda     # Avg. no. of events for Poisson
+
+    # Continuous distributions
+    if (input$distType == "Continuous") {
+
+      # Create continuous histogram
+      hist(dat(), col = "orange", xlab = "Observations",
+           main = paste('r', dist, '(', n, ')', sep=''), prob = TRUE)
+
+
+      # Draw density curve?
+      if (input$density) {
+        lines(density(dat(), adjust = input$bw), lwd = 2)
       }
-      else
-      {
-        poisdat<-rpois(n,lambda)         
-        hist(poisdat,col="orange", main=paste('r', dist, '(', n, ')', sep=''),prob=TRUE)
-        if(input$density == TRUE) lines(density(poisdat,adjust=input$bw),lwd=2)
+
+    # Discrete distributions
+    } else if (input$dist_disc == "bino") {
+
+      # Generate random binomial distribution
+      bindat <- rbinom(n, ntrials, psucc)
+      # Draw binomial histogram
+      hist(bindat, col = "orange",
+           main = paste('r', dist, '(', n, ')', sep=''), prob = TRUE)
+
+      # Draw density curve?
+      if (input$density) {
+        lines(density(bindat, adjust = input$bw), lwd = 2)
+      }
+
+    } else {
+
+      # Generate random Poisson distribution
+      poisdat <- rpois(n, lambda)
+      # Draw Poisson histogram
+      hist(poisdat, col = "orange",
+           main = paste('r', dist, '(', n, ')', sep=''),prob = TRUE)
+
+      # Draw density curve?
+      if (input$density) {
+        lines(density(poisdat, adjust = input$bw), lwd = 2)
       }
     }
   })
-  
+
+  # Tab displaying summary statistics of data set
   output$summary <- renderPrint({
     summary(dat())
   })
-  
+
+  # Tab displaying table with entire data set
   output$table <- renderTable({
-    data.frame(x=dat())
+    data.frame(x = dat())
   })
 })
--- app/ui.R
+++ app/ui.R
@@ -1,49 +1,70 @@
+library(shiny)
+
 shinyUI(pageWithSidebar(
+
+  # Title
   headerPanel("Distributions of Random Variables"),
-  
+
+  # User input panel
   sidebarPanel(
-    
+
+    # Select between types of distributions
     selectInput(
       "distType", "Distribution Type",
-      choices=c("Discrete", "Continuous"), selected="Discrete"
+      choices = c("Discrete", "Continuous"), selected = "Discrete"
     ),
-    
+
+    # Continuous distributions
     conditionalPanel(
       condition = "input.distType == 'Continuous'",
-      radioButtons("dist","Distribution Name:",
-                    list("Normal"="norm","Uniform"="unif","Exponential"="exp")
-                   )
+      radioButtons("dist", "Distribution Name:",
+                   list("Normal"      = "norm",
+                        "Uniform"     = "unif",
+                        "Exponential" = "exp")
+                  )
     ),
-    
+
+    # Discrete distributions
     conditionalPanel(
       condition = "input.distType == 'Discrete'",
-      radioButtons("dist_disc","Distribution Name:",
-                   list("Poisson"="pois","Binomial"="bino")
+      radioButtons("dist_disc", "Distribution Name:",
+                   list("Poisson" = "pois",
+                        "Binomial" = "bino")
       )
     ),
-    sliderInput("n","Sample size:", min=1, max=1000, value=500),
+
+    # Sample size slider
+    sliderInput("n", "Sample size:", min = 1, max = 1000, value = 500),
+
+    # Numeric input boxes for trials and success probability
     conditionalPanel(
-      condition="input.dist_disc=='bino'",
-      numericInput("ntrials","Number of trials:",10),
-      numericInput("psucc","Probability of success:",0.5)
+      condition = "input.dist_disc=='bino'",
+      numericInput("ntrials", "Number of trials:", 10),
+      numericInput("psucc", "Probability of success:", 0.5)
     ),
+
+    # Numeric input box for lambda
     conditionalPanel(
-      condition="input.dist_disc=='pois'",
-      numericInput("lambda","Lambda:",0.5)
+      condition = "input.dist_disc=='pois'",
+      numericInput("lambda", "Lambda:", 0.5)
     ),
-    checkboxInput("density","Show density curve",FALSE),
+
+    # Checkbox governing whether density curve is plotted
+    checkboxInput("density", "Show density curve", FALSE),
+
+    # Numeric input box for density curve bandwidth
     conditionalPanel(
-      condition="input.density==true",
-      numericInput("bw","bandwidth:",1)
+      condition = "input.density==true",
+      numericInput("bw", "bandwidth:", 1)
     )
   ),
-  
+
+  # Tabs
   mainPanel(
-    #plotOutput("plot")
     tabsetPanel(
-      tabPanel("Plot",plotOutput("plot")),
-      tabPanel("Summary",verbatimTextOutput("summary")),
-      tabPanel("Table",tableOutput("table"))
+      tabPanel("Plot",    plotOutput("plot")),
+      tabPanel("Summary", verbatimTextOutput("summary")),
+      tabPanel("Table",   tableOutput("table"))
     )
   )
 ))
